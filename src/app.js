f6c7c89f071806fe711099b684b0185d49230389
alekseev konstantin <alexcseev1990@gmail.com>
2023-07-03 15:37:24 +0300

    add reactive

--- src/app.js
+++ src/app.js
@@ -2,8 +2,12 @@ import { MainView } from "./views/main/main.js";
 
 class App {
     routes = [
-        {path: "", view: MainView}
-    ]
+        {path: "", view: MainView},
+    ];
+
+    appState = {
+        favorites: [],
+    }
     
     constructor() {
         window.addEventListener("hashchange", this.route.bind(this));
@@ -15,7 +19,7 @@ class App {
             this.currentView.destroy();
         }
         const view = this.routes.find(r => r.path == location.hash).view;
-        this.currentView = new view();
+        this.currentView = new view(this.appState);
         this.currentView.render();
     }
 }
