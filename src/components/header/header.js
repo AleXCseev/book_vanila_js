f3d50d19816df8c9aadc492be89b8fbeacb950f7
Kontantin Alekseev <alexcseev1990@gmail.com>
2023-07-06 16:11:34 +0300

    add hader.css

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import { DivComponent } from "../../common/div-component";
+import './header.css';
 
 export class Header extends DivComponent {
     constructor(appState) {
@@ -12,6 +13,19 @@ export class Header extends DivComponent {
         this.el.innerHTML = `
             <div>
                 <img src="./src/static/logo.svg" alt="логотип"/>
+            </div>
+            <div class="menu">
+                <a class="menu__item" href="#">
+                    <img src="./src/static/search.svg" alt="поиск иконка"/>
+                    Поиск книг
+                </a>
+                <a class="menu__item" href="#">
+                    <img src="./src/static/favorites.svg" alt="избранное иконка"/>
+                    Избранное
+                    <div class="menu__counter">
+                        ${this.appState.favorites.length}
+                    </div>
+                </a>
             </div>
         `;
 
